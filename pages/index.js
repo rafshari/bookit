0c4469eb09827c009d3b7fdd7dc44ce18d3cb5a2
Roozbeh <rafshari@gmail.com>
2023-03-13 10:52:16 +0330

    dockerize3

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,21 @@
-import Layout from 'components/layout/Layout';
-import Home from 'components/Home';
-import { wrapper } from 'redux/store';
-import { getAllRoom } from 'redux/actions/roomAction';
+import Layout from 'components/layout/Layout'
+import Home from 'components/Home'
+import { wrapper } from 'redux/store'
+import { getAllRoom } from 'redux/actions/roomAction'
 export default function HomePage() {
-    return (
-        <Layout>
-            <Home />
-        </Layout>
-    );
+  return (
+    <Layout>
+      <Home />
+    </Layout>
+  )
 }
 
-export const getServerSideProps = wrapper.getServerSideProps((store) => async ({ req, query }) => {
-    const { page, location, guestCapacity, category } = query;
-    await store.dispatch(getAllRoom(req, page, location, guestCapacity, category));
-});
+export const getServerSideProps = wrapper.getServerSideProps(
+  (store) =>
+    async ({ req, query }) => {
+      const { page, location, guestCapacity, category } = query
+      await store.dispatch(
+        getAllRoom(req, page, location, guestCapacity, category)
+      )
+    }
+)
