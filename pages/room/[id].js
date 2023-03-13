5a1891002c1ff87a1e19aaae76c3bafd998c5b4a
Roozbeh <rafshari@gmail.com>
2023-03-13 09:28:57 +0330

    dockerize2

--- pages/room/[id].js
+++ pages/room/[id].js
@@ -1,7 +1,7 @@
-import React from "react";
-import Layout from "components/layout/Layout";
-import RoomDetail from "components/room/RoomDetails";
-import { wrapper } from "redux/store";
+import React from 'react'
+import Layout from 'components/layout/Layout'
+import RoomDetail from 'components/room/RoomDetails'
+import { wrapper } from 'redux/store'
 import { getRoom } from 'redux/actions/roomAction'
 
 export default function RoomPage() {
@@ -11,12 +11,12 @@ export default function RoomPage() {
         <RoomDetail />
       </Layout>
     </>
-  );
+  )
 }
 
 export const getServerSideProps = wrapper.getServerSideProps(
   (store) =>
     async ({ req, params }) => {
-      await store.dispatch(getRoom(req, params.id));
+      await store.dispatch(getRoom(req, params.id))
     }
-);
+)
