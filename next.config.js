5b897fd69fef75f4435b75b522593a23dd6bb470
Roozbeh <rafshari@gmail.com>
2023-03-16 23:22:30 +0330

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -7,25 +7,25 @@ const nextConfig = {
 
   env: {
     DB_LOCAL_URI: 'mongodb://127.0.0.1:27017/bookittest',
-    DB_URI: 'mongodb+srv://roozbeh123:roozbeh123@cluster0.skexl.mongodb.net/bookit?retryWrites=true&w=majority',
+    DB_URI: '',
     
 
 
-    CLOUDINARY_CLOUD_NAME: 'dxb4gek9t',
-    CLOUDINARY_API_KEY: '517861677599238',
-    CLOUDINARY_APISECRET: '3f2a8OTskk-3gBKp7S_SX-OAxwQ',
+    CLOUDINARY_CLOUD_NAME: '',
+    CLOUDINARY_API_KEY: '',
+    CLOUDINARY_APISECRET: '',
 
     NEXTAUTH_URL: 'http://localhost:3000',
-    NEXTAUTH_SECRET: '68=UrQZkm2=nSKJMn5*P5qxqU=hrETtXAvzrL#hhjQyp?wE%Z',
+    NEXTAUTH_SECRET: '',
     
     SMTP_HOST: 'sandbox.smtp.mailtrap.io',
     SMTP_PORT: '2525',
-    SMTP_USER: '0449cabf15d1eb',
-    SMTP_PASS: '63868fd7fa6cf5',
+    SMTP_USER: '',
+    SMTP_PASS: '',
     SMTP_FROM_NAME: 'Bookit',
     SMTP_FROM_EMAIL: 'noreply@bootkit.com',
 
-    STRIPE_API_KEY:'pk_test_51MhWPKSIXSENGovq2ECA0fycIowPX9AOshe7oulApe3eNJpkWY2yIuXcH0EOfQPu4blvREI3nGYGytVOyJwCeUOt00QP5pWura',
+    STRIPE_API_KEY:'',
 
     
     STRIPE_SECRET_KEY:'sk_test_51MhWPKSIXSENGovqrLMdPFfNIRpcFEmFKcGEYSiu4Q1CFy2DvAEvQT6dITyHe8VJG998vjwfNaNlbW7v8E8fsTr600IlJvY46L',
