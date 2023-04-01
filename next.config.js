27c280a0b8f1eaad4fc1899628d1f5220357d5e0
Roozbeh Afshari <rafshari@gmail.com>
2023-04-01 21:40:48 +0330

    v.0.0.2

--- next.config.js
+++ next.config.js
@@ -6,7 +6,7 @@ const nextConfig = {
   output: 'standalone',
 
   env: {
-    DB_LOCAL_URI: 'mongodb://127.0.0.1:27017/bookittest',
+   // DB_LOCAL_URI: 'mongodb://127.0.0.1:27017/bookittest',
     DB_URI:
       'mongodb+srv://roozbeh123:roozbeh123@cluster0.skexl.mongodb.net/bookit?retryWrites=true&w=majority',
 
@@ -15,7 +15,7 @@ const nextConfig = {
     CLOUDINARY_APISECRET: '3f2a8OTskk-3gBKp7S_SX-OAxwQ',
 
 
-    //NEXTAUTH_URL: 'http://localhost:3000',
+   // NEXTAUTH_URL: 'http://localhost:3000',
     NEXTAUTH_URL: 'https://bookit-six.vercel.app/',
     NEXTAUTH_SECRET: '68=UrQZkm2=nSKJMn5*P5qxqU=hrETtXAvzrL#hhjQyp?wE%Z',
 
@@ -34,7 +34,8 @@ const nextConfig = {
 
     STRIPE_SECRET_KEY: 'sk_test_51MhWPKSIXSENGovqrLMdPFfNIRpcFEmFKcGEYSiu4Q1CFy2DvAEvQT6dITyHe8VJG998vjwfNaNlbW7v8E8fsTr600IlJvY46L',
 
-    STRIPE_WEBHOOK_SECRET: 'whsec_28f782caa59003e6c39a8e42426e8bff8115e5eb7159ed3ab7ce09656df2f147',
+   // STRIPE_WEBHOOK_SECRET: 'whsec_28f782caa59003e6c39a8e42426e8bff8115e5eb7159ed3ab7ce09656df2f147',
+    STRIPE_WEBHOOK_SECRET: 'whsec_650zs4yK7Q3IzFCXzLHTmUPkKwO9BIMg',
   },
   images: {
     domains: ['res.cloudinary.com', 'images.unsplash.com'],
