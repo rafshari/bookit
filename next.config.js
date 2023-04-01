91d25ff076fe34ffe0b21f2d37471b896665c831
Roozbeh Afshari <rafshari@gmail.com>
2023-04-01 23:33:44 +0330

    v.1.0.2

--- next.config.js
+++ next.config.js
@@ -6,7 +6,7 @@ const nextConfig = {
   output: 'standalone',
 
   env: {
-   // DB_LOCAL_URI: 'mongodb://127.0.0.1:27017/bookittest',
+    // DB_LOCAL_URI: 'mongodb://127.0.0.1:27017/bookittest',
     DB_URI:
       'mongodb+srv://roozbeh123:roozbeh123@cluster0.skexl.mongodb.net/bookit?retryWrites=true&w=majority',
 
@@ -14,13 +14,14 @@ const nextConfig = {
     CLOUDINARY_API_KEY: '517861677599238',
     CLOUDINARY_APISECRET: '3f2a8OTskk-3gBKp7S_SX-OAxwQ',
 
-
-   // NEXTAUTH_URL: 'http://localhost:3000',
+    // NEXTAUTH_URL: 'http://localhost:3000',
     NEXTAUTH_URL: 'https://bookit-six.vercel.app/',
     NEXTAUTH_SECRET: '68=UrQZkm2=nSKJMn5*P5qxqU=hrETtXAvzrL#hhjQyp?wE%Z',
 
-    DATABASE_URL:'mongodb+srv://roozbeh123:roozbeh123@cluster0.skexl.mongodb.net/bookit?retryWrites=true&w=majority', 
-    GOOGLE_ID: '532281339946-m4ubnblu6sp9cgks01fa2kkgs8pl56o7.apps.googleusercontent.com',
+    DATABASE_URL:
+      'mongodb+srv://roozbeh123:roozbeh123@cluster0.skexl.mongodb.net/bookit?retryWrites=true&w=majority',
+    GOOGLE_ID:
+      '532281339946-m4ubnblu6sp9cgks01fa2kkgs8pl56o7.apps.googleusercontent.com',
     GOOGLE_SECRET: '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',
 
     SMTP_HOST: 'sandbox.smtp.mailtrap.io',
@@ -30,12 +31,15 @@ const nextConfig = {
     SMTP_FROM_NAME: 'Bookit',
     SMTP_FROM_EMAIL: 'noreply@bootkit.com',
 
-    STRIPE_API_KEY: 'pk_test_51MhWPKSIXSENGovq2ECA0fycIowPX9AOshe7oulApe3eNJpkWY2yIuXcH0EOfQPu4blvREI3nGYGytVOyJwCeUOt00QP5pWura',
+    STRIPE_API_KEY:
+      'pk_test_51MhWPKSIXSENGovq2ECA0fycIowPX9AOshe7oulApe3eNJpkWY2yIuXcH0EOfQPu4blvREI3nGYGytVOyJwCeUOt00QP5pWura',
 
-    STRIPE_SECRET_KEY: 'sk_test_51MhWPKSIXSENGovqrLMdPFfNIRpcFEmFKcGEYSiu4Q1CFy2DvAEvQT6dITyHe8VJG998vjwfNaNlbW7v8E8fsTr600IlJvY46L',
+    STRIPE_SECRET_KEY:
+      'sk_test_51MhWPKSIXSENGovqrLMdPFfNIRpcFEmFKcGEYSiu4Q1CFy2DvAEvQT6dITyHe8VJG998vjwfNaNlbW7v8E8fsTr600IlJvY46L',
 
-   // STRIPE_WEBHOOK_SECRET: 'whsec_28f782caa59003e6c39a8e42426e8bff8115e5eb7159ed3ab7ce09656df2f147',
-    STRIPE_WEBHOOK_SECRET: 'whsec_650zs4yK7Q3IzFCXzLHTmUPkKwO9BIMg',
+    // STRIPE_WEBHOOK_SECRET: 'whsec_28f782caa59003e6c39a8e42426e8bff8115e5eb7159ed3ab7ce09656df2f147',
+    //STRIPE_WEBHOOK_SECRET: 'whsec_650zs4yK7Q3IzFCXzLHTmUPkKwO9BIMg',
+    STRIPE_WEBHOOK_SECRET: 'whsec_p2yT8O6LC6syPIX0NnV6vYlXuKQvNHzX',
   },
   images: {
     domains: ['res.cloudinary.com', 'images.unsplash.com'],
